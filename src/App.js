da0eed799b980723fea498b29bec82b6ca11d059
pavithranluffy <84136495+Pavithranluffy@users.noreply.github.com>
2025-03-25 19:19:22 +0000

    init

--- src/App.js
+++ src/App.js
@@ -22,15 +22,44 @@ const ProgressBar = ({ progress }) => {
     </div>
   );
 };
+const ProgressBarWidth = ({ progress }) => {
+  const [animationProgressbar, setAnimationPrgress] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimationPrgress(progress);
+    }, 100);
+  }, [progress]);
+  return (
+    <div className="outer-container">
+      <div
+        className="inner-container"
+        style={{
+          width: `${animationProgressbar}%`,
+          //transform: `translateX(${animationProgressbar - 100}%)`,
+        }}
+      >
+        <span className="text-container"> {animationProgressbar}</span>
+      </div>
+    </div>
+  );
+};
 
 export default function App() {
   const BarsList = [0, 3, 5, 10, 20, 30, 60, 85, 100];
   return (
     <div className="App">
-      <h1>Progress Bar</h1>
-      {BarsList.map((items) => (
-        <ProgressBar key={items} progress={items} />
-      ))}
+      <div>
+        <h1>Progress Bar Using TranslateX</h1>
+        {BarsList.map((items) => (
+          <ProgressBar key={items} progress={items} />
+        ))}
+      </div>
+      <div>
+        <h1>Progress Bar Using Width</h1>
+        {BarsList.map((items) => (
+          <ProgressBarWidth key={items} progress={items} />
+        ))}
+      </div>
     </div>
   );
 }
